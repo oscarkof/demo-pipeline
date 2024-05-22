abfa9b11d579f9095fbae815cf9a4ac1a28b714e
Oscar Martinez <osmaroone@gmail.com>
2024-05-22 15:51:52 -0500

    Change Pipeline

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -9,11 +9,21 @@ export class InfraStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'PipelineDemo',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.connection('oscarkof/demo-devops', 'main', {
+        input: CodePipelineSource.connection('oscarkof/demo-pipeline', 'main', {
           connectionArn: 'arn:aws:codestar-connections:us-east-1:702944629921:connection/42daae90-0ffc-4dce-9f91-308a9f2defa4', // Created using the AWS code pipeline console 
         }),
-        commands: ['npm install -g aws-cdk','npm install -D esbuild', 'pwd', 'cd demo-infra', 'npm run prebuild', 'npm run build', 'cdk synth', 'mv cdk.out ../']
+        commands: ['npm install -g aws-cdk','npm install -D esbuild', 'pwd', 'cd demo-infra', 'npm run prebuild', 'cdk synth'],
+        primaryOutputDirectory: 'demo-infra/cdk.out'
       })
     });
+
+    /* const stage = pipeline.addStage(new MyPipelineAppStage(this, 'test', {
+      env: { account: '111111111111', region: 'eu-west-1' }
+    }));
+    
+    stage.addPre(new ShellStep('validate', {
+      input: source,
+      commands: ['sh ../tests/validate.sh']
+    })); */
   }
 }
